a7286dec7f1b077f7dc2580f1bd74e8137d72b9c
ibrahim <orderofside.ik@gmail.com>
2025-12-30 00:10:48 +0300

    model: Question soyut sinifi ve temel ozellikleri eklendi

---
+++ src/Question.java
@@ -0,0 +1,20 @@
+public abstract class Question implements Gradable {
+ private String text;
+ private int points;
+ 
+ public Question(String text, int points) {
+    this.text = text;
+    this.points = points;
+ }
+
+ public abstract boolean checkAnswer(String answer) ;
+
+    public String getText() {
+        return text;    
+ }
+ @Override
+ public int getPoints() {
+    return points;
+ }
+}
+
