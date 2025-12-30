86bde0e3c4b41c9ad9c2f79976914275196b3341
ibrahim <orderofside.ik@gmail.com>
2025-12-30 18:41:25 +0300

    logic: Ogrenci puan hesaplama mantigi (addScore) eklendi

--- src/Student.java
+++ src/Student.java
@@ -1,6 +1,20 @@
 public class Student {
     private String name;
     private String studentId;
-    private int score;    
+    private int score;  
+    
+    public Student(String name, String studentId) {
+        this.name = name;
+        this.studentId = studentId;
+        this.score = 0;
+    }
+
+    public void addScore(int points) {
+        if (points > 0) this.score += points;
+    }
+
+    public String getName() { return name;}
+    public String getStudentId() { return studentId; }
+    public int getScore() { return score;}
 }
 
