fd6815ce028bea501ecb728c6ca97f5293d40623
ibrahim <orderofside.ik@gmail.com>
2025-12-30 18:53:03 +0300

    feat: Soruya siklar (options) listesi eklendi

--- src/MultipleChoiceQuestion.java
+++ src/MultipleChoiceQuestion.java
@@ -1,12 +1,25 @@
 import java.util.ArrayList;
 
 public class MultipleChoiceQuestion extends Question {
-    public MultipleChoiceQuestion(String text, int points) {
+    private ArrayList<String> options;
+    private int correctOptionIndex;
+
+    public MultipleChoiceQuestion(String text, int points, ArrayList<String> options, int correctOptionIndex) {
         super(text, points);
+        this.options = options;
+        this.correctOptionIndex = correctOptionIndex;
     }
 
     @Override
     public boolean checkAnswer(String answer) {
         return false;
     }
+
+    public void displayOptions() {
+        for (int i=0; i < options.size(); i++) {
+            System.out.println((i + 1) + ") " + options.get(i));
+        }
+    }
+
+    public ArrayList<String> getOptions() { return options;}
 }
