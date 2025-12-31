9ea8227d31cf42e10c0187b77acdfeefcc45c959
ibrahim <orderofside.ik@gmail.com>
2025-12-31 15:21:03 +0300

    Quiz sinifi ve soru listesi yapisi kuruldu

---
+++ src/Quiz.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Quiz {
+    private String name;
+    private Difficulty difficulty;
+    private String filename;
+    private ArrayList<Question> questions;
+
+    public Quiz(String name, Difficulty difficulty, String filename) {
+        this.name = name;
+        this.difficulty = difficulty;
+        this.filename = filename;
+        this.questions = new ArrayList<>();
+    }
+
+    public void addQuestion(Question q) {
+        this.questions.add(q);
+    }
+
+    public String getName() {return name;}
+    public Difficulty geDifficulty() {return difficulty;}
+    public String getFilename() {return filename;}
+    public ArrayList<Question> getQuestions() {return questions;}
+    
+
+}
