eb2ee61fbbc529a41d4a4c95bb4d05a1cd701328
ibrahim <orderofside.ik@gmail.com>
2026-01-02 09:57:13 +0300

    Yonetici paneli menu tasarimi yapildi

---
+++ src/AdminPanel.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class AdminPanel {
+    private Scanner scanner;
+    public AdminPanel(Scanner scanner) { this.scanner = scanner;}
+
+    public void start() {
+        System.out.println("Yonetici Menusu");
+        System.out.println("1. Quiz Olustur");
+    }
+}
